b67ef001d56ac7e86c07fc190a29979511b46b02
Rajan Paudel <rajan.rp50@gmail.com>
2021-07-27 19:45:33 +0545

    app released

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+// import 'dart:html';
+// import 'dart:ffi';
+// import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:human_generator_app/drawingarea.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +16,80 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<DrawingArea> points =[];
+  Widget imageOutput;
+
+  void saveToImage (List<DrawingArea> points) async{
+     final recorder = ui.PictureRecorder(); 
+     final canvas =Canvas(recorder,Rect.fromPoints(Offset(0.0,0.0),Offset(200,200)));
+     Paint paint =Paint()
+        ..color =Colors.white
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth =2.0;
+    
+    final paint2 = Paint()
+    ..style = PaintingStyle.fill
+    ..color = Colors.black;
+
+    canvas.drawRect(Rect.fromLTWH(0, 0, 256, 256),paint2 );
+
+    for(int i=0;i<points.length -1;i++){
+      if(points[i] != null && points[i +1] != null){
+        canvas.drawLine(points[i].point, points[i+1].point, paint);
+      }
+    }
+    final picture = recorder.endRecording();
+    final img =await picture.toImage(256, 256);
+
+
+    final pngBytes =await img.toByteData(format : ui.ImageByteFormat.png);
+    final listBytes =  Uint8List.view(pngBytes.buffer);
+
+    // File file = await writeBytes(listBytes) ;
+
+    String base64 =base64Encode(listBytes);
+    fetchResponse(base64);
+
+
+  }
+
+  void fetchResponse(var base64Image) async{
+    var data = {'Image':base64Image};
+
+    print("starting Request");
+
+    var url ='http://192.168.2.15:5000/predict';
+
+    Map<String , String>headers ={
+      'Content-type' :'application/json',
+      'Accept':'application/json',
+      'Connection':'Keep-Alive',
+
+    };
+    var body =json.encode(data);
+    try{
+      var response =  await http.post(url,body:body,headers:headers);
+      final Map<String ,dynamic> responseData = json.decode(response.body);
+      String outputBytes = responseData['Image'];
+      print(outputBytes.substring(2,outputBytes.length -1));
+      displayResponseImage(outputBytes.substring(2,outputBytes.length -1));
+
+    }catch(e){
+      print(" ** Error has occured");
+      return null;
+    }
+  }
+
+  void displayResponseImage(String bytes) async{
+    Uint8List convertedBytes = base64Decode(bytes);
+    setState(() {
+      imageOutput = Container(
+        width: 256,
+        height:256 ,
+        child: Image.memory(convertedBytes,fit:BoxFit.cover,)
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,6 +156,7 @@ class _HomeState extends State<Home> {
                         });
                       },
                        onPanEnd: (details){
+                         saveToImage(points);
                         this.setState(() {
                           points.add(null);
                         });
@@ -107,7 +190,20 @@ class _HomeState extends State<Home> {
                       })
                     ],
                   ),
-                )
+                ),
+
+                Padding(padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Container(
+                  child: Center(
+                    child: Container(
+                      height: 256,
+                      width: 256,
+                      child: imageOutput,
+                    ),
+                  ),
+
+                ),
+                ),
               ],
             ),
           )
